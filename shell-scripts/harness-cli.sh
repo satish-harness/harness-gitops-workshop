adf0c940b2d2ca8f06cd7a8ae298646683b7ea4e
Satish Penugonda <satish.penugonda@harness.io>
2024-08-11 15:58:19 +0100

    updated with github and gitops related values

--- shell-scripts/harness-cli.sh
+++ shell-scripts/harness-cli.sh
@@ -1,10 +1,4 @@
-# Login to harness management workspace
-harness login --api-key pat.cPcMSab0TciUK5YVZSZOrQ.66ab8f356564346813b4bb67.GpjqpLMnMNWMCEUdCfhY --account-id cPcMSab0TciUK5YVZSZOrQ
 
-#Create secrets for github and docker hub
-
-harness secret apply --secret-name "github_pat" --token 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
-harness secret apply --secret-name "docker_secret" --token dckr_pat_v0zSjF_9RB_CMFBuTqK3KEU_FhY
 
 
 # Create Connectors for both github and docker using secrets stored in harness
